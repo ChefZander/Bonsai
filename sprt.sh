85051beacca16baf4744bae447f49d89f4c1bdf4
ChefZander <60350322+ChefZander@users.noreply.github.com>
2026-06-26 22:00:10 +0200

    optimized smallnet inference +160 elo

--- sprt.sh
+++ sprt.sh
@@ -1,8 +1,8 @@
 cutechess-cli \
-  -engine name="Neural SmallNet 5" cmd="./neural-smallnet5" \
-  -engine name="Neural SillyNet 2" cmd="./neural-sillynet2" \
+  -engine name="Optimized SmallNet 5 Inference" cmd="./build/optimized" \
+  -engine name="Neural SillyNet 2" cmd="./build/neural-sillynet2" \
   -each proto=uci st=0.1 timemargin=9999 \
-  -openings file="./sprt/UHO_Lichess_4852_v1.epd" format=epd order=random \
+  -openings file="./data/UHO_Lichess_4852_v1.epd" format=epd order=random \
   -repeat \
   -sprt elo0=0 elo1=5 alpha=0.05 beta=0.05 \
   -concurrency 4 \
